475802e26df139ebf8d69663e97282465273d4b8
cjj <2742895138@qq.com>
2024-03-16 13:53:24 +0800

    添加图表管理页面

--- config/routes.ts
+++ config/routes.ts
@@ -6,18 +6,17 @@ export default [
   },
   { path: '/', redirect:'/add_chart'},
   { path: '/add_chart', name: '智能分析', icon: 'barChart', component: './AddChart' },
-  { path: '/welcome', name: '欢迎', icon: 'smile', component: './Welcome' },
+  { path: '/my_chart', name: '我的图表', icon: 'pieChart', component: './MyChart' },
   {
     path: '/admin',
-    name: '管理页',
+    //name: '管理页',
     icon: 'crown',
     access: 'canAdmin',
     routes: [
-      { path: '/admin', redirect: '/admin/sub-page' },
+      { path: '/admin',name:'管理页面', redirect: '/admin/sub-page' },
       { path: '/admin/sub-page', name: '二级管理页', component: './Admin' },
     ],
   },
-  { name: '查询表格', icon: 'table', path: '/list', component: './TableList' },
   { path: '/', redirect: '/welcome' },
   { path: '*', layout: false, component: './404' },
 ];
---
+++ src/pages/MyChart/index.tsx
@@ -0,0 +1,118 @@
+import { listMyChartByPageUsingPost } from '@/services/LvJiBI/chartController';
+import { Avatar, Card, List, message } from 'antd';
+import React, { useEffect, useState } from 'react';
+import ReactECharts from 'echarts-for-react';
+import { getInitialState } from '@/app';
+import { useModel } from '@umijs/max';
+import Search from 'antd/es/input/Search';
+
+/**
+ * 我的图表页面
+ * @returns 
+ */
+const MyChartPage: React.FC = () => {
+
+  const initSearchParams = {
+    current:1,
+    pageSize: 4,
+  }
+
+  const [searchParams, setsearchParams] = useState<API.ChartQueryRequest>({ ...initSearchParams });
+  const [chartList, setChartList] = useState<API.Chart[]>();
+  const [total, setTotal] = useState<number>(0);
+  const {initialState,setInitialState} = useModel('@@initialState');
+  const {currentUser} = initialState ?? {};
+  const [loading, setLoading] = useState<boolean>(true);
+
+  /**
+   * 调用后端接口获取数据
+   */
+  const loadData = async () => {
+    setLoading(true);
+    try {
+      const res = await listMyChartByPageUsingPost(searchParams);
+      if (res.data) {
+        setChartList(res.data?.records ?? []);
+        setTotal(res.data?.total ?? 0);
+        // 隐藏 genChart 种的 title
+        if (res.data.records) {
+          res.data.records.forEach(data => {
+            const chartOption = JSON.parse(data.genChart ?? '{}');
+            chartOption.title = undefined;
+            data.genChart = JSON.stringify(chartOption);
+          })
+        }
+      } else {
+        message.error("获取我的图表失败");
+      }
+    } catch (error: any) {
+      message.error("获取我的图表失败," + error.message)
+    }
+    setLoading(false);
+  }
+  /**
+   * 页面初次加载、searchParams 发生变化时，都会调用 useEffect 这个钩子函数
+   */
+  useEffect(() => {
+    loadData();
+  }, [searchParams]);
+
+  return (
+    <div className="my-chart-page">
+      <div>
+        <Search placeholder="请输入图表名称" enterButton loading={loading} onSearch={(value) => {
+          // 设置搜索条件
+          setsearchParams ({
+            ...initSearchParams,
+            name: value,
+          })
+        }}/>
+      </div>
+      <div className="margin-16" />
+      <List
+        grid={{
+          gutter: 16,
+          xs: 1,
+          sm: 1,
+          md: 1,
+          lg: 2,
+          xl: 2,
+          xxl: 2,
+        }}
+        // itemLayout="vertical"
+        pagination={{
+          onChange: (page, pageSize) => {
+            setsearchParams ({
+              ...searchParams,
+              current: page,
+              pageSize,
+            })
+          },
+          current: searchParams.current,
+          pageSize: searchParams.pageSize,
+          total: total,
+        }}
+        loading={loading}
+        dataSource={chartList}
+        renderItem={(item) => (
+          <List.Item key={item.id}>
+            <Card style={{width:'100%'}}>
+              <List.Item.Meta
+                avatar={<Avatar src={currentUser && currentUser.userAvatar} />}
+                title={item.name}
+                description={item.chartType ? ('图表类型：' + item.chartType) : undefined}
+              />
+              <div style={{ marginBottom: 16 }} />
+              <p>{'分析目标：' + item.goal}</p>
+              <div style={{ marginBottom: 16 }} />
+              <ReactECharts option={item.genChart && JSON.parse(item.genChart ?? '{}')} />
+            </Card>
+          </List.Item>
+        )}
+      />
+      总数：{total}
+    </div>
+
+  );
+};
+export default MyChartPage;
